5e512e1fa60260a949150b4f9416df28f9fe0fe0
Babar <babarjawad010@gmail.com>
2022-04-01 00:23:39 +0500

    commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function spanevent()
     if(flag%2!=0)
     {
         a.style.display = "block";
-        a.style.zIndex = "100";
+        a.style.zIndex = "1000";
         a.style.width = "50%";
         a.style.borderTopRightRadius="100px"
         a.style.borderBottomRightRadius="100px"
